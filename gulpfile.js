b10fd5fd2a2bcea56433160191a5dbf007ca4214
Raffaele Tufano <pocho962010@hotmail.it>
2020-10-21 23:10:25 +0200

    Add scss compiler and others

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,38 @@
-const gulp = require('gulp');
+const { src, dest } = require("gulp");
 
-const handlebars = require('gulp-compile-handlebars');
+const sass = require("gulp-dart-sass");
 
-const rename = require('gulp-rename');
+const prefix = require("gulp-autoprefixer");
 
-gulp.task('default', function () {
-    return gulp
-        .src('./src/pages/*.hbs')
-        .pipe(handlebars({}, {
-            ignorePartials: true,
-            batch: ['./src/partials']
-        }))
-        .pipe(
-            rename({
-                extname: '.html',
-            })
-        )
-        .pipe(gulp.dest('./dist'));
-});
+const handlebars = require("gulp-compile-handlebars");
+
+const rename = require("gulp-rename");
+
+function scss() {
+  return src("./src/scss/**/*.scss")
+    .pipe(sass())
+    .pipe(prefix())
+    .pipe(dest("./dist/css"));
+}
+
+function hbs() {
+  return src("./src/pages/*.hbs")
+    .pipe(
+      handlebars(
+        {},
+        {
+          ignorePartials: true,
+          batch: ["./src/partials"],
+        }
+      )
+    )
+    .pipe(
+      rename({
+        extname: ".html",
+      })
+    )
+    .pipe(dest("./dist"));
+}
+
+exports.scss = scss;
+exports.hbs = hbs;
