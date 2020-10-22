df0437471e2326644477547e48454b670157beaf
raffa96 <pocho962010@hotmail.it>
2020-10-22 10:27:54 +0200

    Add CSS optimization and others

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,60 @@
-const { src, dest } = require("gulp");
+const {src, dest, watch} = require("gulp");
 
 const sass = require("gulp-dart-sass");
 
 const prefix = require("gulp-autoprefixer");
 
+const csso = require("gulp-csso");
+
+const sourcemaps = require("gulp-sourcemaps");
+
 const handlebars = require("gulp-compile-handlebars");
 
 const rename = require("gulp-rename");
 
+const browserSync = require("browser-sync").create();
+
 function scss() {
-  return src("./src/scss/**/*.scss")
-    .pipe(sass())
-    .pipe(prefix())
-    .pipe(dest("./dist/css"));
+    return src("./src/scss/**/*.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(prefix())
+        .pipe(csso())
+        .pipe(sourcemaps.write("."))
+        .pipe(dest("./dist/css"))
+        .pipe(browserSync.stream());
 }
 
 function hbs() {
-  return src("./src/pages/*.hbs")
-    .pipe(
-      handlebars(
-        {},
-        {
-          ignorePartials: true,
-          batch: ["./src/partials"],
-        }
-      )
-    )
-    .pipe(
-      rename({
-        extname: ".html",
-      })
-    )
-    .pipe(dest("./dist"));
+    return src("./src/pages/*.hbs")
+        .pipe(
+            handlebars(
+                {},
+                {
+                    ignorePartials: true,
+                    batch: ["./src/partials"],
+                }
+            )
+        )
+        .pipe(
+            rename({
+                extname: ".html",
+            })
+        )
+        .pipe(dest("./dist"));
 }
 
 exports.scss = scss;
 exports.hbs = hbs;
+
+exports.default = function () {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+
+    watch("./src/scss/**/*.scss", scss);
+    watch("./src/**/*.hbs", hbs)
+    watch("./dist/*.html").on("change", browserSync.reload);
+};
